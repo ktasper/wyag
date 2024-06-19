2433d6ccc1750cb92bf4d3ef8bbd5879a87e5c8d
Karl Webster <19327709+ktasper@users.noreply.github.com>
2024-06-19 21:55:03 +0100

    Added files

--- tests/functional/test_cli.py
+++ tests/functional/test_cli.py
@@ -1,8 +1,9 @@
 import os
+
 import pytest
 
 
 @pytest.mark.parametrize("test_input,expected_exit_code", [("version", 0), ("", 512)])
 def test_entrypoint(test_input, expected_exit_code):
-    exit_status = os.system(f'./wyag.py {test_input}')
+    exit_status = os.system(f"./wyag.py {test_input}")
     assert exit_status == expected_exit_code
